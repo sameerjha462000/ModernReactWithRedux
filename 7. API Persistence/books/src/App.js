2a4472a9c19125b7c0f30926608039eab2d8e965
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2023-05-04 12:43:48 +0530

    ㅤ

--- 7. API Persistence/books/src/App.js
+++ 7. API Persistence/books/src/App.js
@@ -1,14 +1,30 @@
-import { useState } from 'react';
-import BookCreate from './components/BookCreate';
-import BookList from './components/BookList';
+import { useState, useEffect } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newTitle) => {
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    console.log(response);
+
+    setBooks((books) => response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
       return book;
@@ -17,7 +33,10 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    // before updating the books state, we need to delete it from the database
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -25,14 +44,22 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    // console.log(response);
+
+    // const updatedBooks = [
+    //   ...books,
+    //   {
+    //     id: Math.round(Math.random() * 9999),
+    //     title,
+    //   },
+    // ];
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
