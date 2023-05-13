be51bf0ab4195c43922ae9bbb9d381c17ed63aea
Abhishek jha <46026348+sameerjha462000@users.noreply.github.com>
2023-05-13 20:01:31 +0530

    ㅤ

--- 10. Custom Navigation and Routing Systems/comps/src/App.js
+++ 10. Custom Navigation and Routing Systems/comps/src/App.js
@@ -1,29 +1,35 @@
 import Button from "./components/Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
 const App = () => {
   return (
     <div>
       <div>
         <Button primary rounded outline>
+          <GoBell />
           Click me!!
         </Button>
       </div>
       <div>
         <Button secondary outline>
+          <GoCloudDownload />
           Buy Now!!
         </Button>
       </div>
       <div>
         <Button success rounded>
+          <GoDatabase />
           See Deal!!
         </Button>
       </div>
       <div>
         <Button warning rounded outline>
+          <GoBell />
           Hide Ads!!
         </Button>
       </div>
       <div>
         <Button danger outline>
+          <GoBell />
           Something!!
         </Button>
       </div>
--- 10. Custom Navigation and Routing Systems/comps/src/components/Button.js
+++ 10. Custom Navigation and Routing Systems/comps/src/components/Button.js
@@ -1,4 +1,4 @@
-import classnames from "classnames"; // https://www.npmjs.com/package/classnames
+import achaar from "classnames"; // https://www.npmjs.com/package/classnames
 
 const Button = ({
   children,
@@ -10,22 +10,25 @@ const Button = ({
   outline,
   rounded,
 }) => {
-  const finalClassName = classnames("px-3 py-1.5 border text-white", {
-    "border-blue-500 bg-blue-500": primary,
-    "border-gray-500 bg-gray-500": secondary,
-    "border-green-500 bg-green-500": success,
-    "border-yellow-500 bg-yellow-500": warning,
-    "border-red-500 bg-red-500": danger,
-    "rounded-full": rounded,
-    "bg-white": outline,
-    "text-blue-500": outline && primary,
-    "text-gray-500": outline && secondary,
-    "text-green-500": outline && success,
-    "text-yellow-500": outline && warning,
-    "text-red-500": outline && danger,
-  });
+  const finalClassName = achaar(
+    " flex item-center px-3 py-1.5 border text-white",
+    {
+      "border-blue-500 bg-blue-500": primary,
+      "border-gray-500 bg-gray-500": secondary,
+      "border-green-500 bg-green-500": success,
+      "border-yellow-500 bg-yellow-500": warning,
+      "border-red-500 bg-red-500": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-500": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-500": outline && warning,
+      "text-red-500": outline && danger,
+    }
+  );
 
-  console.log(finalClassName);
+  // console.log(finalClassName);
 
   return <button className={finalClassName}>{children}</button>;
 };
